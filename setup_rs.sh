c370064ed34eb5144c599f25b2c6a84650029da3
Koji Yamada <32894499+ykoji8681@users.noreply.github.com>
2019-09-18 16:13:55 +0900

    Fix mongo3.2 missing repository

--- setup_rs.sh
+++ setup_rs.sh
@@ -44,8 +44,8 @@ a2enmod ssl
 a2ensite stip-rs-ssl
 
 # for MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt update
 apt install -y mongodb-org
 systemctl start mongod
