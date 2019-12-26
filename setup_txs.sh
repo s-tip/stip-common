839e305e987f4fcdc830df29ee3413ca7e80d8c1
Koji Yamada <32894499+ykoji8681@users.noreply.github.com>
2019-12-26 10:02:03 +0900

    Fix python3 invoke error

--- setup_txs.sh
+++ setup_txs.sh
@@ -9,17 +9,20 @@ git clone https://github.com/s-tip/stip-txs.git
 chown -R stip:stip stip-txs
 
 ## apt install
-apt install -y supervisor gunicorn
+apt install -y gunicorn3
 
 ## pip install
+pip3 install supervisor>=4.1.0
+pip3 install gunicorn>=19.7.1
 pip3 install libtaxii
 pip3 install opentaxii==0.1.7
 
 # copy TXS setting
 mkdir -p $INSTALL_DIR/txs
+mkdir -p /etc/supervisor/conf.d
 cp -pr $COMMON_DIR/stip-txs/conf $INSTALL_DIR/txs
 cp -p $COMMON_DIR/stip-txs/init.d/supervisord.conf /etc/supervisor/
-cp -p $COMMON_DIR/stip-txs/init.d/conf.d/stip-taxii-server.conf /etc/supervisor/conf.d/
+cp -p $COMMON_DIR/stip-txs/init.d/conf.d/stip-taxii-server.ini /etc/supervisor/conf.d/
 cp -pr $COMMON_DIR/stip-txs/init.d/conf.d/cert /etc/supervisor/conf.d/
 ln -s $COMMON_DIR/stip-txs/src $INSTALL_DIR/txs/src
 cp -p $VERSION_FILE $INSTALL_DIR/txs/version
