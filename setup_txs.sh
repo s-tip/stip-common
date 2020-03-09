b4f0e993f0fc6f20e08b658d9e5eec4966b45418
Koji Yamada <32894499+ykoji8681@users.noreply.github.com>
2020-03-09 10:58:58 +0900

    Fix copying cert file error

--- setup_txs.sh
+++ setup_txs.sh
@@ -19,12 +19,11 @@ pip3 install opentaxii==0.1.7
 
 # copy TXS setting
 mkdir -p $INSTALL_DIR/txs
-mkdir -p /etc/supervisor/conf.d
+mkdir -p /etc/supervisor/conf.d/cert
 mkdir -p /var/log/supervisor
 cp -pr $COMMON_DIR/stip-txs/conf $INSTALL_DIR/txs
 cp -p $COMMON_DIR/stip-txs/init.d/supervisord.conf /etc/supervisor/
 cp -p $COMMON_DIR/stip-txs/init.d/conf.d/stip-taxii-server.ini /etc/supervisor/conf.d/
-cp -pr $COMMON_DIR/stip-txs/init.d/conf.d/cert /etc/supervisor/conf.d/
 ln -s $COMMON_DIR/stip-txs/src $INSTALL_DIR/txs/src
 cp -p $VERSION_FILE $INSTALL_DIR/txs/version
 chown -R stip:stip $INSTALL_DIR/txs
