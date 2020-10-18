dc3b31022899ff538938687a0d2fa182424acbdc
ta-hiroki <hiroki_ta@fujitsu.com>
2020-10-18 23:57:08 +0000

    Changed the names of constant value to ALL UPPER CASE. (for #67 issue)

--- src/stip/common/tag.py
+++ src/stip/common/tag.py
@@ -1,8 +1,8 @@
 import re
 import stip.common.const as const
 
-sharp_underbar_reg = re.compile('^#_+$')
-sharp_underbar_numeric_reg = re.compile('^#[_0-9０-９]+$')
+SHARP_UNDERBAR_REG = re.compile('^#_+$')
+SHARP_UNDERBAR_NUMERIC_REG = re.compile('^#[_0-9０-９]+$')
 
 
 def is_tag(word):
@@ -12,11 +12,11 @@ def is_tag(word):
         return False
     if len(word) > const.MAX_HASHTAG_LENGTH:
         return False
-    if re.match(sharp_underbar_reg, word):
+    if re.match(SHARP_UNDERBAR_REG, word):
         return False
     if '#' in word[1:]:
         return False
-    if re.match(sharp_underbar_numeric_reg, word):
+    if re.match(SHARP_UNDERBAR_NUMERIC_REG, word):
         return False
     return True
 
