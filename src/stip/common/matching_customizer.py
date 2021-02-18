4f127e558ffff1a52b5fad2df557e2e21e0af710
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2021-02-18 16:54:24 +0900

    Support matching customizer.

---
+++ src/stip/common/matching_customizer.py
@@ -0,0 +1,81 @@
+import json
+
+
+class MatchingCustomizer(object):
+    __instance = None
+    ALLOWD_TYPE = ['Exact']
+
+    @staticmethod
+    def get_instance():
+        if MatchingCustomizer.__instance is None:
+            MatchingCustomizer()
+        return MatchingCustomizer.__instance
+
+    def __init__(self):
+        if MatchingCustomizer.__instance is not None:
+            raise Exception('Use get_instance method.')
+        else:
+            MatchingCustomizer.__instance = self
+        self.conf_json = None
+
+    def init_customizer_conf(self, conf_file_path):
+        with open(conf_file_path, 'r') as fp:
+            j = json.load(fp)
+        matching_patterns = []
+        if 'matching_patterns' in j:
+            for pattern in j['matching_patterns']:
+                matching_pattern = {}
+                if 'name' not in pattern:
+                    print('No name in a matching_pattern. skip!!')
+                    continue
+                for p in matching_patterns:
+                    if p['name'] == pattern['name']:
+                        print('Duplicate a name. skip!!')
+                        continue
+                matching_pattern['name'] = pattern['name']
+                if 'type' not in pattern:
+                    print('No type in a matching_pattern. skip!!')
+                    continue
+                if pattern['type'] not in self.ALLOWD_TYPE:
+                    print('Invalid type. skip!!: ' + pattern['type'])
+                    continue
+                matching_pattern['type'] = pattern['type']
+                if 'targets' not in pattern:
+                    print('No targets in a matching_pattern. skip!!')
+                    continue
+                if not isinstance(pattern['targets'], list):
+                    print('Invalid targets. skip!!')
+                    continue
+                targets = []
+                for target in pattern['targets']:
+                    if 'object' not in target:
+                        print('No object in a target. skip!!')
+                        continue
+                    if not target['object'].startswith('x-'):
+                        print('Invalid object. skip!!: ' + target['object'])
+                        continue
+                    if 'property' not in target:
+                        print('No property in a target. skip!!')
+                        continue
+                    if not target['property'].startswith('x_'):
+                        print('Invalid property. skip!!: ' + target['property'])
+                        continue
+                    l_ = target['property'].split('/')
+                    prop = '--'.join(l_)
+                    type_ = '%s:%s' % (target['object'], prop)
+                    if type_ in targets:
+                        print('Duplicate a matching elemnt. skip!!')
+                        continue
+                    targets.append(type_)
+                if len(targets) < 2:
+                    print('Too few targets in a target. skip!!')
+                    continue
+                matching_pattern['targets'] = targets
+                matching_patterns.append(matching_pattern)
+
+        self.conf_json = {
+            'matching_patterns': matching_patterns
+        }
+
+    def get_matching_patterns(self):
+        return self.conf_json['matching_patterns']
